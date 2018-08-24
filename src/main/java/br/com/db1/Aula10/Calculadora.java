21a9b03abf2530cbd9dda781219d4fd9470d34c3
andredeloque <andredeloque@gmail.com>
2018-08-24 16:15:54 -0300

    aula 10

--- src/main/java/br/com/db1/Aula10/Calculadora.java
+++ src/main/java/br/com/db1/Aula10/Calculadora.java
@@ -2,4 +2,26 @@ package br.com.db1.Aula10;
 
 public class Calculadora {
 
+	private Double resultado;
+	private Double primeiroValor;
+	private Double segundoValor;
+	public Double getResultado() {
+		return resultado;
+	}
+	public void setResultado(Double resultado) {
+		this.resultado = resultado;
+	}
+	public Double getPrimeiroValor() {
+		return primeiroValor;
+	}
+	public void setPrimeiroValor(Double primeiroValor) {
+		this.primeiroValor = primeiroValor;
+	}
+	public Double getSegundoValor() {
+		return segundoValor;
+	}
+	public void setSegundoValor(Double segundoValor) {
+		this.segundoValor = segundoValor;
+	}
+	
 }
