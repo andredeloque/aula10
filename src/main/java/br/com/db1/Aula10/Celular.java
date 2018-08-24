32b95f91bc8eef7ab40fc098b1972ed8895f347e
andredeloque <andredeloque@gmail.com>
2018-08-24 16:04:12 -0300

    aula 10

--- src/main/java/br/com/db1/Aula10/Celular.java
+++ src/main/java/br/com/db1/Aula10/Celular.java
@@ -1,5 +1,47 @@
 package br.com.db1.Aula10;
 
-public class Celular {
+private class Celular {
 
+			private String operadora;
+			public String getOperadora() {
+				return operadora;
+			}
+			public void setOperadora(String operadora) {
+				this.operadora = operadora;
+			}
+
+			private String nome;
+			private String marca;
+			private Boolean dualChip;
+			private Integer tamanhoTela;
+			
+			
+			public String getNome() {
+				return nome;
+			}
+			public void setNome(String nome) {
+				this.nome = nome;
+			}
+			public String getMarca() {
+				return marca;
+			}
+			public void setMarca(String marca) {
+				this.marca = marca;
+			}
+			public Boolean getDualChip() {
+				return dualChip;
+			}
+			public void setDualChip(Boolean dualChip) {
+				this.dualChip = dualChip;
+			}
+			public Integer getTamanhoTela() {
+				return tamanhoTela;
+			}
+			public void setTamanhoTela(Integer tamanhoTela) {
+				this.tamanhoTela = tamanhoTela;
+			}
+			
+			public Boolean  temSinal (){
+				 return Boolean.TRUE;
+			}
 }
---
+++ src/main/java/br/com/db1/Aula10/Tamanho.java
@@ -0,0 +1,5 @@
+package br.com.db1.Aula10;
+
+public enum Tamanho {
+
+}
