1d56c05fe26cd6042ed481bb5a41d786169edf3b
andredeloque <andredeloque@gmail.com>
2018-08-24 16:56:43 -0300

    aula 10

--- src/main/java/br/com/db1/Aula10/Pessoa.java
+++ src/main/java/br/com/db1/Aula10/Pessoa.java
@@ -5,5 +5,20 @@ import java.sql.Date;
 public class Pessoa {
 	private String nome;
 	private Date dataNascimento;
-	
+	private Sexo sexo;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public Enum<Sexo> Sexo (){
+	return sexo.MASCULINO;
+	}
 }
